f52346e7dad61f9f7b3887004e53378efdc6daf7
mappingvermont <charlie.hofmann@gmail.com>
2015-12-21 15:42:41 -0500

    add try/except

--- monitor.py
+++ monitor.py
@@ -8,90 +8,90 @@ import os
 from scrapeMinutes import getMinuteData
 from postToSlack_and_Buffer import *
 
-# try:
+try:
 
-url = 'https://newrepublic.com/minutes/'
-#    print "Using version {}, we have slack token {}, user {} and buffer token {}".format(version, slackToken, slackUser, bufferToken)
+    url = 'https://newrepublic.com/minutes/'
+    #    print "Using version {}, we have slack token {}, user {} and buffer token {}".format(version, slackToken, slackUser, bufferToken)
 
-dbPath = os.path.join(cwd, 'slack.db')
-conn = sqlite3.connect(dbPath)
-curs = conn.cursor()
+    dbPath = os.path.join(cwd, 'slack.db')
+    conn = sqlite3.connect(dbPath)
+    curs = conn.cursor()
 
-minuteList = getMinuteData(url)
+    minuteList = getMinuteData(url)
 
-selectSQL = "SELECT minuteid, title FROM minutes;"
-curs.execute(selectSQL)
+    selectSQL = "SELECT minuteid, title FROM minutes;"
+    curs.execute(selectSQL)
 
-existingMinutes = {x:unicode(y) for (x,y) in curs.fetchall()}
+    existingMinutes = {x:unicode(y) for (x,y) in curs.fetchall()}
 
-for minuteDict in minuteList:
-    try:
-        existingText = existingMinutes[minuteDict['minuteID']]
+    for minuteDict in minuteList:
+        try:
+            existingText = existingMinutes[minuteDict['minuteID']]
 
-        #Need to check here for an updated title, then call update function to buffer
-        if minuteDict['minuteText'] != existingText:
+            #Need to check here for an updated title, then call update function to buffer
+            if minuteDict['minuteText'] != existingText:
 
-            curs.execute("SELECT bufferid FROM minutes WHERE minuteid = ?;", (minuteDict['minuteID'], ))
-            updateBufferID = curs.fetchone()[0]
+                curs.execute("SELECT bufferid FROM minutes WHERE minuteid = ?;", (minuteDict['minuteID'], ))
+                updateBufferID = curs.fetchone()[0]
 
-            newText = minuteDict['minuteText']+minuteDict['minuteURL']
-            bufferDict = updateBufferPost(updateBufferID, newText)
+                newText = minuteDict['minuteText']+minuteDict['minuteURL']
+                bufferDict = updateBufferPost(updateBufferID, newText)
 
-            print "Updated existing post on buffer, success: {}".format(bufferDict['success'])
+                print "Updated existing post on buffer, success: {}".format(bufferDict['success'])
 
-            curs.execute("UPDATE minutes SET title = ? WHERE minuteid = ?", (minuteDict['minuteText'], minuteDict['minuteID']))
-            conn.commit()
+                curs.execute("UPDATE minutes SET title = ? WHERE minuteid = ?", (minuteDict['minuteText'], minuteDict['minuteID']))
+                conn.commit()
 
-        else:
-         #If the minuteID and title match, no reason to update an existing post
-            pass
+            else:
+             #If the minuteID and title match, no reason to update an existing post
+                pass
+                
             
-        
-    #If we haven't already processed this minuteID
-    except KeyError:
-#        print minuteDict
+        #If we haven't already processed this minuteID
+        except KeyError:
+    #        print minuteDict
 
-        bufferDict = postBuffer(minuteDict)
+            bufferDict = postBuffer(minuteDict)
 
-        successText = bufferDict['success']
+            successText = bufferDict['success']
 
-        if successText:
-            print 'Added minuteid {} to buffer!'.format(minuteDict['minuteID'])
+            if successText:
+                print 'Added minuteid {} to buffer!'.format(minuteDict['minuteID'])
 
-            bufferID = bufferDict['updates'][0]['id']
+                bufferID = bufferDict['updates'][0]['id']
 
-            #important due to api rates
-            time.sleep(1)
+                #important due to api rates
+                time.sleep(1)
 
-            insertSQL = """INSERT INTO minutes (minuteid, title, minuteurl, imgurl, under120chars, bufferid, minutesdate) 
-                            VALUES (?, ?, ?, ?, ?, ?, julianday(?))"""
+                insertSQL = """INSERT INTO minutes (minuteid, title, minuteurl, imgurl, under120chars, bufferid, minutesdate) 
+                                VALUES (?, ?, ?, ?, ?, ?, julianday(?))"""
 
-            curs.execute(insertSQL, (minuteDict['minuteID'], minuteDict['minuteText'], minuteDict['minuteURL'], 
-                                      minuteDict['imgURL'], minuteDict['under120chars'], bufferID, datetime.date.today()))
+                curs.execute(insertSQL, (minuteDict['minuteID'], minuteDict['minuteText'], minuteDict['minuteURL'], 
+                                          minuteDict['imgURL'], minuteDict['under120chars'], bufferID, datetime.date.today()))
 
-            conn.commit()
+                conn.commit()
 
-        else:
-            print bufferDict
-            print 'here'
-            subprocess.call('mail -s "bufferdict msg: {0}" charlie.hofmann@gmail.com < /dev/null'.format(bufferDict['message']), shell=True)
+            else:
+                print bufferDict
+                print 'here'
+                subprocess.call('mail -s "bufferdict msg: {0}" charlie.hofmann@gmail.com < /dev/null'.format(bufferDict['message']), shell=True)
 
 
-#Delete old minutes from the system
-curs.execute("DELETE FROM minutes WHERE julianday() - minutesdate > 3;")
+    #Delete old minutes from the system
+    curs.execute("DELETE FROM minutes WHERE julianday() - minutesdate > 3;")
 
-conn.commit()
+    conn.commit()
 
-#Check that the script is running all the way through, and email me
-currentTime = datetime.datetime.now().time()
+    #Check that the script is running all the way through, and email me
+    currentTime = datetime.datetime.now().time()
 
-if currentTime.hour == 1 and currentTime.minute == 1:
-    subprocess.call('mail -s "minutes script works" charlie.hofmann@gmail.com < /dev/null', shell=True)
-else:
-    pass
+    if currentTime.hour == 1 and currentTime.minute == 1:
+        subprocess.call('mail -s "minutes script works" charlie.hofmann@gmail.com < /dev/null', shell=True)
+    else:
+        pass
 
-# except Exception as e:
-#     exc_type, exc_obj, exc_tb = sys.exc_info()
-#     print exc_type, exc_tb.tb_lineno
-#     subprocess.call('mail -s "error in minutes script" charlie.hofmann@gmail.com < /dev/null', shell=True)
-#     print 'Error in minutes script'
+except Exception as e:
+    exc_type, exc_obj, exc_tb = sys.exc_info()
+    print exc_type, exc_tb.tb_lineno
+    subprocess.call('mail -s "error in minutes script" charlie.hofmann@gmail.com < /dev/null', shell=True)
+    print 'Error in minutes script'
